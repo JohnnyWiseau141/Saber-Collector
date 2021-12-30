e177ecc67c7f745f017ae499b7849052df2ecdc2
JohnnyWiseau141 <john.a.foley141@gmail.com>
2021-12-30 18:14:40 -0500

    fitting default saber icons to index

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('sabers/', views.sabers_index, name='cats_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,20 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def sabers_index(request):
+  return render(request, 'sabers/index.html', { 'sabers': sabers})
+
+class Saber:
+  def __init__(self, owner, color, hilt, blades):
+    self.owner = owner
+    self.color = color
+    self.hilt = hilt
+    self.blades = blades
+
+sabers = [
+  Saber('Cal Kestis', 'blue', 'custom', 2),
+  Saber('Obi-wan', 'blue', 'round-bottom', 1),
+  Saber('Maul', 'red', 'long and techie', 2),
+  Saber('Rey Skywalker', 'yellow', 'Black copy of Maul\'s hilt', 1),
+]
