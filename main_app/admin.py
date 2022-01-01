888dca70fd948054192b3d042eff02ddbde01aef
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-01-01 17:33:08 -0500

    view photos on index

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Saber, Repairing, Crystal
+from .models import Saber, Repairing, Crystal, Photo
 
 # Register your models here.
 admin.site.register(Saber)
 admin.site.register(Repairing)
 admin.site.register(Crystal)
+admin.site.register(Photo)
